78b643275f9eba72cb8e7632d60ae83738b30613
weikentchen89 <113404804+weikentchen89@users.noreply.github.com>
2023-02-16 17:21:12 -0600

    Add files via upload

---
+++ solution/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "dac3859238616b097b1b85e9f72062cf"
+
+# Geoapify API Key
+geoapify_key = "804e022d8a14461fbd60ac974db95807"
+
+#Google Maps
+gkey = "AIzaSyCAXlw76l7MJaiWhFcdWFO7wOOi6O36TF0"
+
